09c967cfa0d9fb4a08b014b36c951a25e32aab02
Muchowkey <muchokibenson60@gmail.com>
2024-03-28 09:25:27 +0300

    added tile buttons to dashboard.dart

--- lib/src/features/core/dashboard.dart
+++ lib/src/features/core/dashboard.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    OutlinedButton(
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(LineAwesomeIcons.plus),
+                              Text(
+                                'Create',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Text('Specify Contribution'),
+                            ],
+                          ),
+                        )),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    OutlinedButton(
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(LineAwesomeIcons.plus),
+                              Text(
+                                'Create',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Text('Specify Contribution'),
+                            ],
+                          ),
+                        )),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    OutlinedButton(
+                        onPressed: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(LineAwesomeIcons.plus),
+                              Text(
+                                'Create',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Text('Specify Contribution'),
+                            ],
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 4,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Transactions'),
+                      Text('See all'),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
